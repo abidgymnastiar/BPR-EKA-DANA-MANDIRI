a254f608a05cfca3b7c840d8dfadab2039f110d8
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-19 15:43:08 +0700

    chore: Update admin layout with HTML structure and dependencies

---
+++ resources/views/components/admin/nav.blade.php
@@ -0,0 +1,63 @@
+<nav class="fixed z-30 w-full bg-gray-50">
+    <div class="py-3 px-3 lg:px-5 lg:pl-3">
+        <div class="flex justify-between items-center">
+            <div class="flex justify-start items-center">
+                <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar"
+                    class="p-2 mr-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100">
+                    <svg id="toggleSidebarMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <svg id="toggleSidebarMobileClose" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <a href="{{ route('admin.dashboard') }}" class="text-md font-semibold flex items-center lg:mr-1.5">
+                    <img src="{{ asset('assets/admin/images/logo.svg') }}" class="mr-2 h-8" alt="Creative Tim Logo">
+                    <span class="hidden md:inline-block self-center text-xl font-bold whitespace-nowrap">Admin</span>
+                </a>
+
+            </div>
+            <div class="flex items-center">
+                <button id="toggleSidebarMobileSearch" type="button">
+                </button>
+                <button data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start"
+                    class="cursor-pointer" type="button">
+                    <i class="fa fa-user-circle h-10 w-10" id="avatarButton" aria-hidden="true"></i>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="userDropdown"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                        <div>Bonnie Green</div>
+                        <div class="font-medium truncate">name@flowbite.com</div>
+                    </div>
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                        </li>
+                    </ul>
+                    <div class="py-1">
+                        <a href="#"
+                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                            out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
--- resources/views/components/layout/admin.blade.php
+++ resources/views/components/layout/admin.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Get started with a free dashboard template featuring hundreds of UI components and pages based on the Soft UI Design System, Flowbite, and Tailwind CSS">
+    <meta name="author" content="Creative Tim">
+    <meta name="generator" content="Hugo 0.127.0">
+
+    <title>Admin</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('assets/admin/app.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js"
+        integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    @vite([])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+</head>
+
+<body class="bg-gray-50">
+    <x-admin.nav />
+    <x-admin.sidebar />
+    {{ $slot }}
+</body>
+
+<script src="{{ asset('assets/admin/app.bundle.js') }}"></script>
+
+</html>
