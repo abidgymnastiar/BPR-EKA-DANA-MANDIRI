e8ccfcc3fd24f671458712c1d537397ec8212527
abidgymnastiar <abidgymnastiar@gmail.com>
2024-06-24 12:12:42 +0700

    home update

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -23,6 +23,7 @@
             </div>
         </div>
     </div>
+
     <section class="kredit max-w-screen-xl mx-auto mt-5">
         <div class="grid grid-cols-2 items-center gap-x-5">
             <div class="p-3">
@@ -32,7 +33,7 @@
                     kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
                     Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
                     Kredit Bank.</p>
-                <ul class=" space-y-3.5 text-black font-sans list-inside dark:text-gray-400 text-base mb-9">
+                <ul class=" space-y-3.5 text-black font-sans list-inside dark:text-gray-400 text-base mb-9 font-medium">
                     <li class="flex items-center">
                         <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -70,6 +71,120 @@
             </div>
         </div>
 
+    </section>
+
+    <section class="Deposito max-w-screen-xl mx-auto mt-5">
+        <div class="grid grid-cols-2 items-center gap-x-5">
+            <div class="image p-3">
+                <img class="w-[540px]"
+                    src="https://softivuspro.com/wp/bankio/wp-content/uploads/2023/12/feature-item-1-1-4.png"
+                    alt="">
+            </div>
+            <div class="p-3">
+                <h3 class="text-lg font-medium font-sans text-red-700">Kredit Bank</h3>
+                <h1 class="text-[44px] font-sans font-extrabold mb-2 text-red-800">Pinjaman Cepat dan Mudah</h1>
+                <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
+                    kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
+                    Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
+                    Kredit Bank.</p>
+                <ul class=" space-y-3.5 text-black font-sans list-inside dark:text-gray-400 text-base mb-9 font-medium">
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Dapatkan persetujuan kredit dalam waktu singkat.
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Manfaatkan suku bunga yang kompetitif untuk pinjaman Anda.
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Pilihan tenor yang fleksibel sesuai kebutuhan Anda.
+                    </li>
+                </ul>
+                <button type="button"
+                    class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pesan
+                    Sekarang</button>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="Tabungan max-w-screen-xl mx-auto mt-5">
+        <div class="grid grid-cols-2 items-center gap-x-5">
+            <div class="p-3">
+                <h3 class="text-lg font-medium font-sans text-red-700">Kredit Bank</h3>
+                <h1 class="text-[44px] font-sans font-extrabold mb-2 text-red-800">Pinjaman Cepat dan Mudah</h1>
+                <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
+                    kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
+                    Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
+                    Kredit Bank.</p>
+                <ul class=" space-y-3.5 text-black font-sans list-inside dark:text-gray-400 text-base mb-9 font-medium">
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Dapatkan persetujuan kredit dalam waktu singkat.
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Manfaatkan suku bunga yang kompetitif untuk pinjaman Anda.
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Pilihan tenor yang fleksibel sesuai kebutuhan Anda.
+                    </li>
+                </ul>
+                <button type="button"
+                    class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pesan
+                    Sekarang</button>
+            </div>
+
+            <div class="image p-3">
+                <img class="w-[540px]"
+                    src="https://softivuspro.com/wp/bankio/wp-content/uploads/2023/12/feature-item-1-1-4.png"
+                    alt="">
+            </div>
+        </div>
+
+    </section>
+
+    <section class="Manfaat max-w-screen-xl mx-auto mt-5 p-3">
+        <div class=" text-center">
+            <h3 class="text-lg font-medium font-sans text-red-700 mb-3">Sekilas Tentang Kami</h3>
+            <h1 class="text-center font-bold text-4xl sm:text-5xl leading-tight dark:text-whiten">Mengenal Lebih Dekat
+                Bank Edanan</h1>
+            <p class="mt-8 text-center mx-auto lg:w-auto md:w-1/2">
+                Selamat datang di Bank Edanan (BPR EKA DANA MANDIRI), mitra finansial terpercaya Anda yang berkomitmen
+                untuk memberikan solusi perbankan yang inovatif dan aman. Dengan pengalaman bertahun-tahun di industri
+                perbankan, kami bangga dapat melayani kebutuhan keuangan Anda melalui berbagai produk dan layanan yang
+                dirancang khusus untuk memenuhi kebutuhan individu maupun bisnis. Dedikasi kami terhadap pelayanan prima
+                dan kepercayaan nasabah menjadi dasar dari setiap langkah kami untuk membantu Anda mencapai tujuan
+                finansial dengan lebih mudah dan efisien. Bersama Bank Edanan, kami wujudkan masa depan finansial yang
+                lebih cerah.
+            </p>
+        </div>
     </section>
     <div class="p-8"></div>
 </x-layout.user>
