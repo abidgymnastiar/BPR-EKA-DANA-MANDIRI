45f11b5a2275015e5ed2ed5784e95ee525730d62
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-06-25 16:14:33 +0700

    chore: fix routing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\User\PeminjamanController;
 use Illuminate\Support\Facades\Route;
-
+Route::namespace('Admin')->prefix('admin')->middleware('auth')->name('admin.')->group(base_path('routes/admin.php'));
 
 Route::get('/', function () {
     return redirect()->route('home');
@@ -21,3 +22,6 @@ Route::get('/home', function () {
 Route::post('/peminjaman', [PeminjamanController::class, 'create'])->name('peminjaman.create');
 Route::get('/peminjaman/show/{id}', [PeminjamanController::class, 'show'])->name('peminjaman.show');
 Route::delete('/peminjaman/delete/{id}', [PeminjamanController::class, 'delete'])->name('peminjaman.delete');
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard')->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
