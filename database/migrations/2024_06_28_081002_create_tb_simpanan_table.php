fa6092c034eeeb8d93d726883a5738b41b29dd0c
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-08-02 20:46:50 +0700

    chore: rename field to no_hp

--- database/migrations/2024_06_28_081002_create_tb_simpanan_table.php
+++ database/migrations/2024_06_28_081002_create_tb_simpanan_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tb_simpanan', function (Blueprint $table) {
             $table->id();
             $table->string('nama_lengkap');
-            $table->string('no_telepon');
+            $table->string('no_hp');
             $table->string('email');
             $table->string('provinsi');
             $table->string('kota');
