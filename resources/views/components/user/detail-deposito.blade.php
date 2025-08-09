79d12b097a1474f214e9582d4d262aec34f866b6
abidgymnastiar <abidgymnastiar@gmail.com>
2025-08-09 19:46:24 +0700

    Implement code changes to enhance functionality and improve performance

--- resources/views/components/user/detail-deposito.blade.php
+++ resources/views/components/user/detail-deposito.blade.php
@@ -4,19 +4,23 @@
 
     <section class="kredit max-w-screen-xl mx-auto">
         <div class="image">
-            <img class="w-full" src="{{ asset('assets/user/detail/kredit.png') }}" alt="">
+            <img class="w-full" src="{{ asset('assets/user/detail/deposito.png') }}" alt="">
         </div>
     </section>
 
     <section class="max-w-screen-xl mx-auto">
             <div class="p-3">
                 {{-- <h3 class="text-lg font-sans font-extrabold mb-2 text-red-800">Fasilitas Kredit yang Diberikan oleh BPR Edaman</h3> --}}
-                <p class="text-[#2c3655] text-base tracking-wide">Fasilitas kredit yang diberikan kepada masyarakat dengan penghasilan tetap atau profesi tertentu untuk pembiayaan kebutuhan konsumtif, baik pengajuan baru maupun take over top-up fasilitas kredit yang sudah dimiliki.</p>
+                <p class="text-[#2c3655] text-base tracking-wide">DepositoGo adalah solusi deposito online inovatif dari BPR Edaman yang memberikan kemudahan dan keuntungan lebih bagi Anda. Dengan DepositoGo, Anda dapat membuka deposito kapan saja dan di mana saja hanya melalui aplikasi EdamanMobile.
+
+Nikmati berbagai keunggulan yang ditawarkan, seperti bunga yang kompetitif, proses yang cepat, dan keamanan transaksi yang terjamin. Tidak perlu repot mengunjungi kantor cabang, karena segala proses dapat Anda lakukan secara praktis langsung dari perangkat Anda.
+
+Mulai perencanaan keuangan yang lebih menguntungkan dengan DepositoGo dari BPR Edaman. Deposito menjadi lebih mudah, fleksibel, dan menguntungkan. DepositoGo — Solusi Deposito Cerdas di Genggaman Anda.</p>
     </section>
 
     <section class="kredit max-w-screen-xl mx-auto">
         <div class="p-3">
-            <p class="text-xl font-sans font-extrabold mb-2 text-red-800">KEUNGGULAN KREDIT BPR EDAMAN</p>
+            <p class="text-xl font-sans font-extrabold mb-2 text-red-800">KEUNGGULAN</p>
             {{-- <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
                     kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
                     Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
@@ -28,7 +32,7 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Bunga terjangkau
+                        Bunga lebih tinggi (dibanding bank umum lain)
                     </li>
                     <li class="flex items-center">
                         <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -36,20 +40,20 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Jangka waktu lebih panjang
+                        Mudah pembukaan DepositoGo dari mana saja, tanpa harus ke bank
                     </li>
                     <li class="flex items-center">
-                        <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
+                        <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Bisa mengajukan plafond lebih besar
+                        Pencairan sewaktu-waktu bebas penalty
                     </li>
             </ul>
         </div>
         <div class="p-3">
-            <p class="text-xl font-sans font-extrabold mb-4 text-red-800">SKEMA KREDIT BPR EDAMAN</p>
+            <p class="text-xl font-sans font-extrabold mb-4 text-red-800">SKEMA DEPOSITO BPR EDAMAN</p>
             {{-- <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
                     kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
                     Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
@@ -61,7 +65,7 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Suku bunga mulai 1.25% per bulan
+                        Penempatan deposito mulai dari Rp 1.000.000,-
                     </li>
                 <li class="flex items-center">
                     <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -69,7 +73,7 @@
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    Jangka waktu kredit hingga 20 tahun
+                    Jangka waktu simpanan: 1, 3, 6, 12 bulan
                 </li>
                 <li class="flex items-center">
                     <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -77,37 +81,13 @@
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    Pembiayaan: Tanah dan Bangunan 60%
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Usia debitur minimal 21 tahun dan pada saat jatuh tempo kredit / maksimal 1 tahun sebelum pensiun. Khusus untuk wiraswasta dan profesional, usia maksimal saat jatuh tempo kredit adalah 70 tahun.
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Diskon biaya provisi 50% untuk take over kredit dari bank lain
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Take over ke bank lain dikenakan penalti 5%
+                    Dana aman, dijamin LPS sampai dengan 2 miliar
                 </li>
             </ul>
         </div>
         <button type="button" onclick="window.location.href='https://wa.me/+6281331573454'"
         class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-    Ajukan kredit
+    Ajukan Deposito
 </button>
 
     </section>
--- resources/views/components/user/detail-tabungan.blade.php
+++ resources/views/components/user/detail-tabungan.blade.php
@@ -4,7 +4,7 @@
 
     <section class="kredit max-w-screen-xl mx-auto">
         <div class="image">
-            <img class="w-full" src="{{ asset('assets/user/detail/kredit.png') }}" alt="">
+            <img class="w-full" src="{{ asset('assets/user/detail/tabungan.png') }}" alt="">
         </div>
     </section>
 
@@ -16,7 +16,7 @@
 
     <section class="kredit max-w-screen-xl mx-auto">
         <div class="p-3">
-            <p class="text-xl font-sans font-extrabold mb-2 text-red-800">KEUNGGULAN KREDIT BPR EDAMAN</p>
+            <p class="text-xl font-sans font-extrabold mb-2 text-red-800">KEUNGGULAN TABUNGAN BPR EDAMAN</p>
             {{-- <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
                     kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
                     Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
@@ -28,7 +28,7 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Bunga terjangkau
+                       Bebas biaya administrasi bulanan
                     </li>
                     <li class="flex items-center">
                         <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -36,7 +36,7 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Jangka waktu lebih panjang
+                        Bebas biaya transfer ke bank mana saja di seluruh Indonesia
                     </li>
                     <li class="flex items-center">
                         <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
@@ -44,12 +44,28 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Bisa mengajukan plafond lebih besar
+                        Bebas biaya RTGS
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Bunga kompetitif
+                    </li>
+                    <li class="flex items-center">
+                        <svg class="w-5 h-w-5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Diskon di ratusan merchant LestariDiskon
                     </li>
             </ul>
         </div>
         <div class="p-3">
-            <p class="text-xl font-sans font-extrabold mb-4 text-red-800">SKEMA KREDIT BPR EDAMAN</p>
+            <p class="text-xl font-sans font-extrabold mb-4 text-red-800">SKEMA TABUNGAN BPR EDAMAN</p>
             {{-- <p class="text-[#2c3655] text-base tracking-wide mb-7">Nikmati layanan kredit kami dengan suku bunga
                     kompetitif dan persyaratan yang mudah. Dari kredit pribadi hingga kredit usaha, kami siap membantu
                     Anda mencapai tujuan finansial Anda. Dapatkan akses cepat dan aman ke dana yang Anda butuhkan dengan
@@ -61,7 +77,7 @@
                             <path
                                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Suku bunga mulai 1.25% per bulan
+                        Tanpa setoran awal
                     </li>
                 <li class="flex items-center">
                     <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -69,7 +85,7 @@
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    Jangka waktu kredit hingga 20 tahun
+                   Saldo minimum Rp. 100.000,-
                 </li>
                 <li class="flex items-center">
                     <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -77,37 +93,13 @@
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
-                    Pembiayaan: Tanah dan Bangunan 60%
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Usia debitur minimal 21 tahun dan pada saat jatuh tempo kredit / maksimal 1 tahun sebelum pensiun. Khusus untuk wiraswasta dan profesional, usia maksimal saat jatuh tempo kredit adalah 70 tahun.
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Diskon biaya provisi 50% untuk take over kredit dari bank lain
-                </li>
-                <li class="flex items-center">
-                    <svg class="w-5 h-w-5 me-2 text-red-400 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    Take over ke bank lain dikenakan penalti 5%
+                    Rekening akan ditutup ketika tidak melakukan setoran selama 7 hari sejak pembukaan rekening
                 </li>
             </ul>
         </div>
         <button type="button" onclick="window.location.href='https://wa.me/+6281331573454'"
         class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-    Ajukan kredit
+    Ajukan Tabungan
 </button>
 
     </section>
