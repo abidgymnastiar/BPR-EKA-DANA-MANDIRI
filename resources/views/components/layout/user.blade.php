ad86eb462e0ce15d27d961c35e738c921935f448
abidgymnastiar <abidgymnastiar@gmail.com>
2025-08-10 12:34:51 +0700

    Implement code changes to enhance functionality and improve performance

--- resources/views/components/layout/user.blade.php
+++ resources/views/components/layout/user.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }}</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    {{-- <title>{{ config('app.name') }}</title> --}}
+    <title>BPR Edaman</title>
+    <link rel="shortcut icon" href="{{ asset('logo/logo-icon.png') }}" type="image/x-icon">
     {{-- <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css') }}"> --}}
     {{-- <script src="{{ asset('assets/js/fontawesome.min.js') }}"></script> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
